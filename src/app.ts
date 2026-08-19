671c07c449a9ffa752130de7ef46e9c5c466bff5
muchlass19 <muchlas148@gmail.com>
2026-08-19 18:11:21 +0700

    update cors origin true

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,8 @@ const app: Express = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: true,
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
